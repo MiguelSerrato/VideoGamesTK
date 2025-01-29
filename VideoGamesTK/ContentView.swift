13c9a0e3c21cc0ad36e912cba110acc67841eba6
Miguel Serrato <pau.jmsm@gmail.com>
2025-01-28 22:12:34 -0600

    Upload Project

---
+++ VideoGamesTK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoGamesTK
+//
+//  Created by Jose Miguel Serrato Moreno on 28/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoGamesTK/VideoGamesTKApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoGamesTKApp.swift
+//  VideoGamesTK
+//
+//  Created by Jose Miguel Serrato Moreno on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct VideoGamesTKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
